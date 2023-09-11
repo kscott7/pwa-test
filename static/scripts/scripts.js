2d226019cc0e0126e055a8e1a0eed4a78bff99b8
Kieron Scott <hello@greatscott.design>
2023-09-11 15:20:33 +0100

    Update scripts.js

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -4,12 +4,12 @@ if (navigator.userAgent.match(/iPhone/i)) {
   // do iPhone stuff
   console.log("iPhone");
   document.body.classList.add("ios");
-  content.innerHTML = `<a href="/ios/">Get started!</a>`;
+  content.innerHTML = `<a href="/pwa-test/ios/">Get started!</a>`;
 } else if (navigator.userAgent.match(/Android/i)) {
   // do Android stuff
   console.log("Android");
   document.body.classList.add("android");
-  content.innerHTML = `<a href="/android/">Get started!</a>`;
+  content.innerHTML = `<a href="/pwa-test/android/">Get started!</a>`;
 } else {
   // do desktop stuff
   console.log("Something else");
